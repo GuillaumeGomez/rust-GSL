6d379426d8acdf06fcc94aaaca4f19c891d765e3
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-09-07 00:48:59 +0200

    Use bindgen to generate C files

--- gsl-sys/build.rs
+++ gsl-sys/build.rs
@@ -1,5 +1,3 @@
-extern crate pkg_config;
-
 fn main() {
     if std::process::Command::new("pkg-config").output().is_err() {
         println!("cargo:rustc-link-lib=gsl");
@@ -7,11 +5,13 @@ fn main() {
         return;
     }
 
-    pkg_config::probe_library("gsl")
-        // .map(|lib| {
-        //     if lib.version.starts_with("2.") && ::std::env::var_os("IGNORE_VERSION").is_none() {
-        //         println!(r#"cargo:rustc-cfg=feature="v2""#);
-        //     }
-        // })
-        .expect("GSL library not found");
+    // pkg_config::probe_library("gsl")
+    //     // .map(|lib| {
+    //     //     if lib.version.starts_with("2.") && ::std::env::var_os("IGNORE_VERSION").is_none() {
+    //     //         println!(r#"cargo:rustc-cfg=feature="v2""#);
+    //     //     }
+    //     // })
+    //     .expect("GSL library not found");
+    // pkg_config::probe_library("gslcblas")
+    //     .expect("GSL cblas library not found");
 }
