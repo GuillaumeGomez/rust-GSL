44e5433e79289fb23f11a158ba441c1d688b6967
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-10-02 23:41:42 +0200

    Fix build

--- gsl-sys/build.rs
+++ gsl-sys/build.rs
@@ -1,17 +1,14 @@
+extern crate pkg_config;
+
 fn main() {
     if std::process::Command::new("pkg-config").output().is_err() {
         println!("cargo:rustc-link-lib=gsl");
-        println!("cargo:rustc-link-lib=gslcblas");
+        // println!("cargo:rustc-link-lib=gslcblas");
         return;
     }
 
-    // pkg_config::probe_library("gsl")
-    //     // .map(|lib| {
-    //     //     if lib.version.starts_with("2.") && ::std::env::var_os("IGNORE_VERSION").is_none() {
-    //     //         println!(r#"cargo:rustc-cfg=feature="v2""#);
-    //     //     }
-    //     // })
-    //     .expect("GSL library not found");
-    // pkg_config::probe_library("gslcblas")
-    //     .expect("GSL cblas library not found");
+    pkg_config::probe_library("gsl")
+        .expect("GSL library not found");
+    /*pkg_config::probe_library("gslcblas")
+        .expect("GSL cblas library not found");*/
 }
