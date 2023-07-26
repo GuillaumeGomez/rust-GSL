0dfb90d098d57ffe6653360e42c6861f6d8fd4bf
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-07-26 21:33:37 +0200

    Correctly handle features for examples

--- examples/fitreg2.rs
+++ examples/fitreg2.rs
@@ -4,115 +4,129 @@
 
 extern crate rgsl;
 
-use rgsl::{multifit, MatrixF64, MultifitLinearWorkspace, VectorF64};
+#[cfg(feature = "v2_5")]
+mod example {
+    use rgsl::{multifit, MatrixF64, MultifitLinearWorkspace, VectorF64};
 
-const N: usize = 10; // number of observations
-const P: usize = 8; // number of model parameters
-const NPOINTS: usize = 200; // number of points on L-curve and GCV curve
+    const N: usize = 10; // number of observations
+    const P: usize = 8; // number of model parameters
+    const NPOINTS: usize = 200; // number of points on L-curve and GCV curve
 
-fn hibert_matrix() -> Option<MatrixF64> {
-    let mut x = MatrixF64::new(N, P)?;
-    let n = x.size1();
-    let m = x.size2();
+    fn hibert_matrix() -> Option<MatrixF64> {
+        let mut x = MatrixF64::new(N, P)?;
+        let n = x.size1();
+        let m = x.size2();
 
-    for i in 0..n {
-        for j in 0..m {
-            x.set(i, j, 1. / ((i + j) as f64 + 1.));
+        for i in 0..n {
+            for j in 0..m {
+                x.set(i, j, 1. / ((i + j) as f64 + 1.));
+            }
         }
+        Some(x)
     }
-    Some(x)
+
+    pub fn run() {
+        let mut y = VectorF64::new(N).expect("MatrixF64::new failed");
+
+        // construct Hilbert matrix and rhs vector
+        let mut x = hibert_matrix().expect("hibert_matrix failed");
+
+        let mut val = 1.;
+        for i in 0..N {
+            y.set(i, val);
+            val *= -1.;
+        }
+
+        let mut w =
+            MultifitLinearWorkspace::new(N, P).expect("MultifitLinearWorkspace::new failed");
+        // OLS solution
+        let mut c = VectorF64::new(P).expect("VectorF64::new");
+        // regularized solution (L-curve)
+        let mut c_lcurve = VectorF64::new(P).expect("VectorF64::new");
+        // regularized solution (GCV)
+        let mut c_gcv = VectorF64::new(P).expect("VectorF64::new");
+
+        let mut reg_param = VectorF64::new(NPOINTS).expect("VectorF64::new");
+        // residual norms
+        let mut rho = VectorF64::new(NPOINTS).expect("VectorF64::new");
+        // solution norms
+        let mut eta = VectorF64::new(NPOINTS).expect("VectorF64::new");
+        // GCV function values
+        let mut g = VectorF64::new(NPOINTS).expect("VectorF64::new");
+
+        // compute SVD of X
+        w.linear_svd(&mut x).unwrap();
+
+        let rcond = w.linear_rcond();
+        eprintln!("matrix condition number = {}", 1. / rcond);
+        eprintln!();
+
+        // unregularized (standard) least squares fit, lambda = 0
+        let (rnorm, snorm) = w.linear_solve(0., &x, &y, &mut c).unwrap();
+        let chisq = rnorm.powi(2);
+
+        eprintln!("\n=== Unregularized fit ===");
+        eprintln!("residual norm = {}", rnorm);
+        eprintln!("solution norm = {}", snorm);
+        eprintln!("chisq/dof = {}", chisq / (N - P) as f64);
+
+        // calculate L-curve and find its corner
+        w.linear_lcurve(&y, &mut reg_param, &mut rho, &mut eta)
+            .unwrap();
+        let reg_idx = multifit::linear_lcorner(&rho, &eta).unwrap();
+
+        // store optimal regularization parameter
+        let lambda_l = reg_param.get(reg_idx);
+
+        // regularize with lambda_l
+        let (rnorm, snorm) = w.linear_solve(lambda_l, &x, &y, &mut c_lcurve).unwrap();
+        let chisq = rnorm.powi(2) + (lambda_l * snorm).powi(2);
+
+        eprintln!("\n=== Regularized fit (L-curve) ===");
+        eprintln!("optimal lambda: {}", lambda_l);
+        eprintln!("residual norm = {}", rnorm);
+        eprintln!("solution norm = {}", snorm);
+        eprintln!("chisq/dof = {}", chisq / (N - P) as f64);
+
+        // calculate GCV curve and find its minimum
+        let (lambda_gcv, g_gcv) = w.linear_gcv(&y, &mut reg_param, &mut g).unwrap();
+
+        // regularize with lambda_gcv
+        let (rnorm, snorm) = w.linear_solve(lambda_gcv, &x, &y, &mut c_gcv).unwrap();
+        let chisq = rnorm.powi(2) + (lambda_gcv * snorm).powi(2);
+
+        eprintln!("\n=== Regularized fit (GCV) ===\n");
+        eprintln!("optimal lambda: {}", lambda_gcv);
+        eprintln!("best fit: y = {} u + {} v", c_gcv.get(0), c_gcv.get(1));
+        eprintln!("residual norm = {}", rnorm);
+        eprintln!("solution norm = {}", snorm);
+        eprintln!("chisq/dof = {}", chisq / (N - P) as f64);
+
+        // output L-curve and GCV curve
+        for i in 0..NPOINTS {
+            println!(
+                "{:.6} {:.6} {:.6} {:.6}",
+                reg_param.get(i),
+                rho.get(i),
+                eta.get(i),
+                g.get(i)
+            );
+        }
+
+        // output L-curve corner point
+        println!("\n\n{:.6} {:.6}", rho.get(reg_idx), eta.get(reg_idx));
+
+        // output GCV curve corner minimum
+        println!("\n\n{:.6} {:.6}", lambda_gcv, g_gcv);
+    }
 }
 
+#[cfg(feature = "v2_5")]
 fn main() {
-    let mut y = VectorF64::new(N).expect("MatrixF64::new failed");
+    example::run();
+}
 
-    // construct Hilbert matrix and rhs vector
-    let mut x = hibert_matrix().expect("hibert_matrix failed");
-
-    let mut val = 1.;
-    for i in 0..N {
-        y.set(i, val);
-        val *= -1.;
-    }
-
-    let mut w = MultifitLinearWorkspace::new(N, P).expect("MultifitLinearWorkspace::new failed");
-    // OLS solution
-    let mut c = VectorF64::new(P).expect("VectorF64::new");
-    // regularized solution (L-curve)
-    let mut c_lcurve = VectorF64::new(P).expect("VectorF64::new");
-    // regularized solution (GCV)
-    let mut c_gcv = VectorF64::new(P).expect("VectorF64::new");
-
-    let mut reg_param = VectorF64::new(NPOINTS).expect("VectorF64::new");
-    // residual norms
-    let mut rho = VectorF64::new(NPOINTS).expect("VectorF64::new");
-    // solution norms
-    let mut eta = VectorF64::new(NPOINTS).expect("VectorF64::new");
-    // GCV function values
-    let mut g = VectorF64::new(NPOINTS).expect("VectorF64::new");
-
-    // compute SVD of X
-    w.linear_svd(&mut x).unwrap();
-
-    let rcond = w.linear_rcond();
-    eprintln!("matrix condition number = {}", 1. / rcond);
-    eprintln!();
-
-    // unregularized (standard) least squares fit, lambda = 0
-    let (rnorm, snorm) = w.linear_solve(0., &x, &y, &mut c).unwrap();
-    let chisq = rnorm.powi(2);
-
-    eprintln!("\n=== Unregularized fit ===");
-    eprintln!("residual norm = {}", rnorm);
-    eprintln!("solution norm = {}", snorm);
-    eprintln!("chisq/dof = {}", chisq / (N - P) as f64);
-
-    // calculate L-curve and find its corner
-    w.linear_lcurve(&y, &mut reg_param, &mut rho, &mut eta)
-        .unwrap();
-    let reg_idx = multifit::linear_lcorner(&rho, &eta).unwrap();
-
-    // store optimal regularization parameter
-    let lambda_l = reg_param.get(reg_idx);
-
-    // regularize with lambda_l
-    let (rnorm, snorm) = w.linear_solve(lambda_l, &x, &y, &mut c_lcurve).unwrap();
-    let chisq = rnorm.powi(2) + (lambda_l * snorm).powi(2);
-
-    eprintln!("\n=== Regularized fit (L-curve) ===");
-    eprintln!("optimal lambda: {}", lambda_l);
-    eprintln!("residual norm = {}", rnorm);
-    eprintln!("solution norm = {}", snorm);
-    eprintln!("chisq/dof = {}", chisq / (N - P) as f64);
-
-    // calculate GCV curve and find its minimum
-    let (lambda_gcv, g_gcv) = w.linear_gcv(&y, &mut reg_param, &mut g).unwrap();
-
-    // regularize with lambda_gcv
-    let (rnorm, snorm) = w.linear_solve(lambda_gcv, &x, &y, &mut c_gcv).unwrap();
-    let chisq = rnorm.powi(2) + (lambda_gcv * snorm).powi(2);
-
-    eprintln!("\n=== Regularized fit (GCV) ===\n");
-    eprintln!("optimal lambda: {}", lambda_gcv);
-    eprintln!("best fit: y = {} u + {} v", c_gcv.get(0), c_gcv.get(1));
-    eprintln!("residual norm = {}", rnorm);
-    eprintln!("solution norm = {}", snorm);
-    eprintln!("chisq/dof = {}", chisq / (N - P) as f64);
-
-    // output L-curve and GCV curve
-    for i in 0..NPOINTS {
-        println!(
-            "{:.6} {:.6} {:.6} {:.6}",
-            reg_param.get(i),
-            rho.get(i),
-            eta.get(i),
-            g.get(i)
-        );
-    }
-
-    // output L-curve corner point
-    println!("\n\n{:.6} {:.6}", rho.get(reg_idx), eta.get(reg_idx));
-
-    // output GCV curve corner minimum
-    println!("\n\n{:.6} {:.6}", lambda_gcv, g_gcv);
+#[cfg(not(feature = "v2_5"))]
+fn main() {
+    eprintln!("You need to enable the `v2_5` feature to be able to run this example");
 }
--- examples/largefit.rs
+++ examples/largefit.rs
@@ -4,171 +4,184 @@
 
 extern crate rgsl;
 
-use rgsl::{
-    blas, error, MatrixF64, MultilargeLinearType, MultilargeLinearWorkspace, Rng, RngType,
-    VectorF64,
-};
+#[cfg(feature = "v2_2")]
+mod example {
+    use rgsl::{
+        blas, error, MatrixF64, MultilargeLinearType, MultilargeLinearWorkspace, Rng, RngType,
+        VectorF64,
+    };
 
-// number of observations
-const N: usize = 50000;
-// polynomial order + 1
-const P: usize = 16;
-// regularization parameter
-const LAMBDA: f64 = 0.;
-// number of blocks to accumulate
-const NBLOCK: usize = 5;
-// number of rows per block
-const NROWS: usize = N / NBLOCK;
-const NLCURVE: usize = 200;
-const DT: f64 = 1. / (N as f64 - 1.);
+    // number of observations
+    const N: usize = 50000;
+    // polynomial order + 1
+    const P: usize = 16;
+    // regularization parameter
+    const LAMBDA: f64 = 0.;
+    // number of blocks to accumulate
+    const NBLOCK: usize = 5;
+    // number of rows per block
+    const NROWS: usize = N / NBLOCK;
+    const NLCURVE: usize = 200;
+    const DT: f64 = 1. / (N as f64 - 1.);
 
-fn func(t: f64) -> f64 {
-    let x = (10. * t).sin();
-    (x * x * x).exp()
-}
-
-fn build_row(t: f64, row: &mut VectorF64) {
-    let p = row.len();
-    let mut xj = 1.;
-
-    for j in 0..p {
-        row.set(j, xj);
-        xj *= t;
+    fn func(t: f64) -> f64 {
+        let x = (10. * t).sin();
+        (x * x * x).exp()
     }
-}
 
-fn solve_system(print_data: bool, t: MultilargeLinearType, c: &mut VectorF64) {
-    let mut w =
-        MultilargeLinearWorkspace::new(t, P).expect("MultilargeLinearWorkspace::new failed");
-    let mut x = MatrixF64::new(NROWS, P).expect("MatrixF64::new failed");
-    let mut y = VectorF64::new(NROWS).expect("VectorF64::new failed");
-    let mut r = Rng::new(RngType::default()).expect("Rng::new failed");
+    fn build_row(t: f64, row: &mut VectorF64) {
+        let p = row.len();
+        let mut xj = 1.;
 
-    let mut reg_param = VectorF64::new(NLCURVE).expect("VectorF64::new failed");
-    let mut rho = VectorF64::new(NLCURVE).expect("VectorF64::new failed");
-    let mut eta = VectorF64::new(NLCURVE).expect("VectorF64::new failed");
+        for j in 0..p {
+            row.set(j, xj);
+            xj *= t;
+        }
+    }
 
-    let mut rowidx = 0;
-    let mut t = 0.;
+    fn solve_system(print_data: bool, t: MultilargeLinearType, c: &mut VectorF64) {
+        let mut w =
+            MultilargeLinearWorkspace::new(t, P).expect("MultilargeLinearWorkspace::new failed");
+        let mut x = MatrixF64::new(NROWS, P).expect("MatrixF64::new failed");
+        let mut y = VectorF64::new(NROWS).expect("VectorF64::new failed");
+        let mut r = Rng::new(RngType::default()).expect("Rng::new failed");
 
-    while rowidx < N {
-        // number of rows left to accumulate
-        let nleft = N - rowidx;
-        // number of rows in this block
-        let nr = if NROWS > nleft { nleft } else { NROWS };
+        let mut reg_param = VectorF64::new(NLCURVE).expect("VectorF64::new failed");
+        let mut rho = VectorF64::new(NLCURVE).expect("VectorF64::new failed");
+        let mut eta = VectorF64::new(NLCURVE).expect("VectorF64::new failed");
 
-        let mut xv = x.submatrix(0, 0, nr, P);
-        let mut yv = y.subvector(0, nr);
+        let mut rowidx = 0;
+        let mut t = 0.;
 
-        // build (X,y) block with 'nr' rows
-        for i in 0..nr {
-            xv.matrix_mut(|mat| {
-                mat.expect("Failed to get matrix").row(i, |row| {
-                    let mut row = row.expect("Failed to get row...");
-                    let fi = func(t);
-                    // noise
-                    let ei = r.gaussian(0.1 * fi);
-                    let yi = fi + ei;
+        while rowidx < N {
+            // number of rows left to accumulate
+            let nleft = N - rowidx;
+            // number of rows in this block
+            let nr = if NROWS > nleft { nleft } else { NROWS };
 
-                    // construct this row of LS matrix
-                    row.vector_mut(|vector| {
-                        build_row(t, vector.expect("Failed to get vector"));
+            let mut xv = x.submatrix(0, 0, nr, P);
+            let mut yv = y.subvector(0, nr);
+
+            // build (X,y) block with 'nr' rows
+            for i in 0..nr {
+                xv.matrix_mut(|mat| {
+                    mat.expect("Failed to get matrix").row(i, |row| {
+                        let mut row = row.expect("Failed to get row...");
+                        let fi = func(t);
+                        // noise
+                        let ei = r.gaussian(0.1 * fi);
+                        let yi = fi + ei;
+
+                        // construct this row of LS matrix
+                        row.vector_mut(|vector| {
+                            build_row(t, vector.expect("Failed to get vector"));
+                        });
+
+                        // set right hand side value with added noise
+                        yv.vector_mut(|vector| {
+                            vector.expect("Failed to get vector").set(i, yi);
+                        });
+
+                        if print_data && i % 100 == 0 {
+                            println!("{} {}", t, yi);
+                        }
                     });
 
-                    // set right hand side value with added noise
-                    yv.vector_mut(|vector| {
-                        vector.expect("Failed to get vector").set(i, yi);
-                    });
-
-                    if print_data && i % 100 == 0 {
-                        println!("{} {}", t, yi);
-                    }
+                    t += DT;
                 });
+            }
 
-                t += DT;
+            // accumulate (X,y) block into LS system
+            xv.matrix_mut(|matrix| {
+                yv.vector_mut(|vector| {
+                    w.accumulate(
+                        matrix.expect("Failed to get matrix"),
+                        vector.expect("Failed to get vector"),
+                    )
+                    .unwrap();
+                });
             });
+
+            rowidx += nr;
         }
 
-        // accumulate (X,y) block into LS system
-        xv.matrix_mut(|matrix| {
-            yv.vector_mut(|vector| {
-                w.accumulate(
-                    matrix.expect("Failed to get matrix"),
-                    vector.expect("Failed to get vector"),
-                )
-                .unwrap();
-            });
-        });
+        if print_data {
+            println!();
+            println!();
+        }
 
-        rowidx += nr;
+        // compute L-curve
+        if let Err(e) = w.lcurve(&mut reg_param, &mut rho, &mut eta) {
+            eprintln!(
+                "=== Method {} FAILED ===",
+                w.name().expect("Failed to get name")
+            );
+            eprintln!("error: {:?}", e);
+            return;
+        }
+
+        // solve large LS system and store solution in c
+        let (rnorm, snorm) = w.solve(LAMBDA, c).unwrap();
+
+        // compute reciprocal condition number
+        let rcond = w.rcond().unwrap();
+
+        eprintln!("=== Method {} ===\n", w.name().expect("Failed to get name"));
+        eprintln!("condition number = {}", 1. / rcond);
+        eprintln!("residual norm    = {}", rnorm);
+        eprintln!("solution norm    = {}", snorm);
+
+        // output L-curve
+        for i in 0..NLCURVE {
+            println!(
+                "{:.12} {:.12} {:.12}",
+                reg_param.get(i),
+                rho.get(i),
+                eta.get(i)
+            );
+        }
     }
 
-    if print_data {
+    pub fn run() {
+        let mut c_tsqr = VectorF64::new(P).expect("VectorF64::new failed");
+        let mut c_normal = VectorF64::new(P).expect("VectorF64::new failed");
+
+        // turn off error handler so normal equations method won't abort
+        error::set_error_handler_off();
+
+        // solve system with TSQR method
+        solve_system(true, MultilargeLinearType::tsqr(), &mut c_tsqr);
+
         println!();
         println!();
-    }
 
-    // compute L-curve
-    if let Err(e) = w.lcurve(&mut reg_param, &mut rho, &mut eta) {
-        eprintln!(
-            "=== Method {} FAILED ===",
-            w.name().expect("Failed to get name")
-        );
-        eprintln!("error: {:?}", e);
-        return;
-    }
+        // solve system with Normal equations method
+        solve_system(false, MultilargeLinearType::normal(), &mut c_normal);
 
-    // solve large LS system and store solution in c
-    let (rnorm, snorm) = w.solve(LAMBDA, c).unwrap();
+        // output solution
+        let mut v = VectorF64::new(P).expect("VectorF64::new failed");
+        let mut t = 0.;
 
-    // compute reciprocal condition number
-    let rcond = w.rcond().unwrap();
+        while t <= 1. {
+            let f_exact = func(t);
+            build_row(t, &mut v);
 
-    eprintln!("=== Method {} ===\n", w.name().expect("Failed to get name"));
-    eprintln!("condition number = {}", 1. / rcond);
-    eprintln!("residual norm    = {}", rnorm);
-    eprintln!("solution norm    = {}", snorm);
+            let f_tsqr = blas::level1::ddot(&v, &c_tsqr).unwrap();
+            let f_normal = blas::level1::ddot(&v, &c_normal).unwrap();
 
-    // output L-curve
-    for i in 0..NLCURVE {
-        println!(
-            "{:.12} {:.12} {:.12}",
-            reg_param.get(i),
-            rho.get(i),
-            eta.get(i)
-        );
+            println!("{} {:.6} {:.6} {:.6}", t, f_exact, f_tsqr, f_normal);
+
+            t += 0.01;
+        }
     }
 }
 
+#[cfg(feature = "v2_2")]
 fn main() {
-    let mut c_tsqr = VectorF64::new(P).expect("VectorF64::new failed");
-    let mut c_normal = VectorF64::new(P).expect("VectorF64::new failed");
+    crate::example::run();
+}
 
-    // turn off error handler so normal equations method won't abort
-    error::set_error_handler_off();
-
-    // solve system with TSQR method
-    solve_system(true, MultilargeLinearType::tsqr(), &mut c_tsqr);
-
-    println!();
-    println!();
-
-    // solve system with Normal equations method
-    solve_system(false, MultilargeLinearType::normal(), &mut c_normal);
-
-    // output solution
-    let mut v = VectorF64::new(P).expect("VectorF64::new failed");
-    let mut t = 0.;
-
-    while t <= 1. {
-        let f_exact = func(t);
-        build_row(t, &mut v);
-
-        let f_tsqr = blas::level1::ddot(&v, &c_tsqr).unwrap();
-        let f_normal = blas::level1::ddot(&v, &c_normal).unwrap();
-
-        println!("{} {:.6} {:.6} {:.6}", t, f_exact, f_tsqr, f_normal);
-
-        t += 0.01;
-    }
+#[cfg(not(feature = "v2_2"))]
+fn main() {
+    eprintln!("You need to enable the `v2_2` feature to be able to run this example");
 }
