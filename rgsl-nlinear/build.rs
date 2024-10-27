74e9426bfef8e6039de4825bd3eee29a493ef752
lmandres@yahoo.com <lmandres@yahoo.com>
2024-10-27 16:51:53 -0400

    Remove rgsl-cblib and move code to rgsl-nlinear.

--- rgsl-nlinear/build.rs
+++ rgsl-nlinear/build.rs
@@ -1,19 +1,17 @@
 extern crate pkg_config;
 
+use std::path::Path;
+
+
 fn main() {
+
     cc::Build::new()
         .file("src/multifit_nlinear.c")
-        .include("./rgsl-cblib/target/debug")
-        .flag("-lrgsl_cblib")
         .compile("rgslmfnlin");
-
-    println!("cargo:rustc-link-search=./target/debug");
-    println!("cargo:rustc-link-search=./rgsl-cblib/target/debug");
 
     if std::process::Command::new("pkg-config").output().is_err() {
         println!("cargo:rustc-link-lib=gsl");
         println!("cargo:rustc-link-lib=gslcblas");
-        println!("cargo:rustc-link-lib=rgsl_cblib");
         println!("cargo:rustc-link-lib=rgslmfnlin");
         return;
     }
@@ -23,9 +21,6 @@ fn main() {
     }
     if pkg_config::probe_library("gslcblas").is_err() {
         println!("cargo:rustc-link-lib=gslcblas");
-    }
-    if pkg_config::probe_library("rgsl_cblib").is_err() {
-        println!("cargo:rustc-link-lib=rgsl_cblib");
     }
     if pkg_config::probe_library("rgslmfnlin").is_err() {
         println!("cargo:rustc-link-lib=rgslmfnlin");
